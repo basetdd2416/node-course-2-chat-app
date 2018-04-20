95d72401888ac71940db8f3b4420c8008b1b7a41
Apisit Onakekasit <blue@appsynth.net>
2018-04-20 15:52:07 +0700

    add test package

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
+const { generateMessage } = require('./utils/message')
 const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000
 const app = express();
@@ -21,17 +22,8 @@ io.on('connection', (socket) => {
     console.log('createEmail', newEmail)
   })
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to chat app',
-    createdAt: new Date().getTime()
-  })
-
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  })
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'))
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 })
 
 app.use(express.static(publicPath))
